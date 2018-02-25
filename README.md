3d6f7798e5d502501fff111cf6a975bad116118c
greatjack1 <yaakovrim@gmail.com>
2018-02-25 15:42:44 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sushi
+
+A interpreted programming language based on the jvm built by myself as a excersize
+
+## Notice
+
+This is currently a work in progress which will be updated as I have free time
---
+++ src/com/wyre/sushi/Main.java
@@ -0,0 +1,51 @@
+package com.wyre.sushi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+static boolean hadError;
+    public static void main(String[] args) throws IOException {
+	if(args.length >1){
+	    System.out.println("Usage: sushi [script]");
+    } else if(args.length==1){
+	    runFile(args[0]);
+    } else{
+        runPrompt();
+    }
+    }
+    public static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
+    }
+    public static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+        for(;;){
+            System.out.println("> ");
+            run(reader.readLine());
+            hadError = false;
+        }
+    }
+    public static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token :tokens){
+            System.out.println(token);
+        }
+    }
+    static void error(int line,String message){
+        report(line,"",message);
+    }
+    private static void report(int line,String where,String message){
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+}
